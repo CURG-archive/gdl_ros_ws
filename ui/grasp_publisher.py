e4ab8c7153ef378c303cfbdfaa832de60ab54ee1
Jared Weiss <jared@jaredweiss.net>
2015-01-09 15:12:03 -0500

    Updated publish_grasp to use geometry_msgs/Transform instead of Pose

--- ui/grasp_publisher.py
+++ ui/grasp_publisher.py
@@ -1,6 +1,7 @@
 from rospy import Publisher
 from sensor_msgs.msg import JointState
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Transform
 
 class GraspPublisher():
 
@@ -9,9 +10,20 @@ class GraspPublisher():
         self.hand_pose_topic = hand_pose_topic
 
         self.gdl_joint_states_pub = Publisher(self.joint_state_topic, JointState, queue_size=10)
-        self.gdl_hand_pose_pub = Publisher(self.hand_pose_topic, Pose, queue_size=10)
+        self.gdl_hand_pose_pub = Publisher(self.hand_pose_topic, Transform, queue_size=10)
 
     def publish_grasp(self,grasp):
         # Publish grasp info on rostopics "gdl_joint_states" and "gdl_robot_pose"
         self.gdl_joint_states_pub.publish(grasp.joint_values)
-        self.gdl_hand_pose_pub.publish(grasp.pose)
+        pose = grasp.pose
+
+        tf = Transform()
+        tf.rotation = pose.orientation
+
+        translation = Vector3()
+        translation.x = pose.position.x
+        translation.y = pose.position.y
+        translation.z = pose.position.z
+        tf.translation = translation
+
+        self.gdl_hand_pose_pub.publish(tf)
